e4a9ae2e003f69c0001fe462006bdad78e7af3cb
Simon <simonausbs@gmail.com>
2023-10-09 15:50:34 +0200

    Initial commit

---
+++ src/client/java/com/github/curryp0mmes/tpll/BtetpllplusClient.java
@@ -0,0 +1,10 @@
+package com.github.curryp0mmes.tpll;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BtetpllplusClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
